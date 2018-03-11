94e5ce366e3b75e00f13745859f0449cf2ad8622
emrsn-SebastianVancea <sebastian.vancea@emerson.com>
2018-03-11 22:44:04 +0100

    Initial Commit

---
+++ Nennos-Pizza-iOSTests/Nennos_Pizza_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Nennos_Pizza_iOSTests.swift
+//  Nennos-Pizza-iOSTests
+//
+//  Created by Sebastian Vancea on 11/03/2018.
+//  Copyright © 2018 Sebastian Vancea. All rights reserved.
+//
+
+import XCTest
+@testable import Nennos_Pizza_iOS
+
+class Nennos_Pizza_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
